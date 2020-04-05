e5b6be9c601fda5515db92487fc0ba718a4eb495
liupeng2013cim2 <605538074@qq.com>
2020-04-05 14:57:58 +0800

    uploade files

---
+++ ndk/cmake-build-debug/CMakeFiles/ndk.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ndk.dir/main.c.obj"
+  "libndk.dll.a"
+  "ndk.exe"
+  "ndk.exe.manifest"
+  "ndk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ndk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ndk/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    unsigned short u1 = 2;
+    printf("u1=%u\n", u1);
+    return 0;
+}
